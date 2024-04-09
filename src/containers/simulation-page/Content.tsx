7a2f9e91aae8e98d86a6921cd4da515b8e9b23d3
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-04-09 17:52:27 +0300

    Fix height issue

--- src/containers/simulation-page/Content.tsx
+++ src/containers/simulation-page/Content.tsx
@@ -1,7 +1,7 @@
 import { parseColor } from "@/lib/utils";
 import { SimulationQueryResult } from "sanity.types";
 import { urlForImage } from "./../../../sanity/lib/image";
-import { Box, Container, Grid, styled } from "styled-system/jsx";
+import { Box, Container, Flex, Grid, styled } from "styled-system/jsx";
 import Image from "next/image";
 import { RichText } from "@/components/RichText";
 
@@ -15,86 +15,88 @@ export const Content = ({
 
   return (
     <styled.section>
-      <Container
-        maxW="1239px"
-        display="flex"
-        justifyContent="space-between"
-        gap="40px"
-        flexDir={{ base: "column-reverse", md: "row" }}
-        w="full"
-      >
-        <styled.div w="full" maxW="600px">
-          <>
-            <styled.h1 fontSize="28px" fontWeight="extrabold">
-              {simulation?.title}
-            </styled.h1>
-
-            <styled.div
-              mt="24px"
-              rounded="16px"
-              py="2px"
-              px="8px"
-              fontSize="xs"
-              style={{
-                backgroundColor: bgColor,
-                color: textColor,
-              }}
-              fontWeight="black"
-              display="inline-block"
-              textTransform="uppercase"
-            >
-              {simulation?.topic?.title}
-            </styled.div>
-
-            <styled.p fontWeight="semibold" mt="40px" color="gray">
-              {simulation?.subtitle}
-            </styled.p>
-          </>
-
-          <styled.div mt="80px">
-            <styled.h2
-              fontSize="sm"
-              fontWeight="black"
-              textTransform="uppercase"
-              mb="24px"
-            >
-              Objectives
-            </styled.h2>
-
-            {/* @ts-ignore */}
-            <RichText content={simulation?.objectives} />
-          </styled.div>
-        </styled.div>
-
-        <Grid
-          gridTemplateColumns={{ base: 1, md: 2 }}
-          rowGap="8px"
-          columnGap="10px"
+      <Container maxW="1239px" display="flex" w="full">
+        <Flex
           w="full"
+          justifyContent="space-between"
+          gap="40px"
+          flexDir={{ base: "column-reverse", md: "row" }}
         >
-          {simulation?.simulationScreenshots?.map((screenshot) => {
-            return (
+          <styled.div w="full" maxW="600px">
+            <>
+              <styled.h1 fontSize="28px" fontWeight="extrabold">
+                {simulation?.title}
+              </styled.h1>
+
               <styled.div
-                key={screenshot._key}
-                pos="relative"
-                height="177px"
-                w="full"
-                rounded="10px"
+                mt="24px"
+                rounded="16px"
+                py="2px"
+                px="8px"
+                fontSize="xs"
+                style={{
+                  backgroundColor: bgColor,
+                  color: textColor,
+                }}
+                fontWeight="black"
+                display="inline-block"
+                textTransform="uppercase"
               >
-                <Image
-                  src={urlForImage(screenshot)}
-                  alt={screenshot.alt || ""}
-                  fill
-                  style={{
-                    backgroundColor: bgColor,
-                    objectFit: "cover",
-                    borderRadius: "12px",
-                  }}
-                />
+                {simulation?.topic?.title}
               </styled.div>
-            );
-          })}
-        </Grid>
+
+              <styled.p fontWeight="semibold" mt="40px" color="gray">
+                {simulation?.subtitle}
+              </styled.p>
+            </>
+
+            <styled.div mt="80px">
+              <styled.h2
+                fontSize="sm"
+                fontWeight="black"
+                textTransform="uppercase"
+                mb="24px"
+              >
+                Objectives
+              </styled.h2>
+
+              {/* @ts-ignore */}
+              <RichText content={simulation?.objectives} />
+            </styled.div>
+          </styled.div>
+
+          <Grid
+            gridTemplateColumns={{ base: 1, md: 2 }}
+            rowGap="8px"
+            columnGap="10px"
+            w="full"
+            h="fit-content"
+          >
+            {simulation?.simulationScreenshots?.map((screenshot) => {
+              return (
+                <styled.div
+                  key={screenshot._key}
+                  pos="relative"
+                  height="177px"
+                  w="full"
+                  rounded="10px"
+                  border="1px solid rgba(0, 0, 0, 0.1)"
+                >
+                  <Image
+                    src={urlForImage(screenshot)}
+                    alt={screenshot.alt || ""}
+                    fill
+                    style={{
+                      backgroundColor: bgColor,
+                      objectFit: "cover",
+                      borderRadius: "12px",
+                    }}
+                  />
+                </styled.div>
+              );
+            })}
+          </Grid>
+        </Flex>
       </Container>
     </styled.section>
   );
