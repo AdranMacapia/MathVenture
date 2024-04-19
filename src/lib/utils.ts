939f61d7d57c646997f127e7deb7e3b76ed72e8f
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-04-19 04:03:28 +0100

    Add fallback from protocol

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,5 +4,5 @@ export const parseColor = (color?: Color["rgb"] | null) => {
   return `rgba(${color?.r}, ${color?.g}, ${color?.b}, ${color?.a})`;
 };
 
-export const baseUrl = `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+export const baseUrl = `${process.env.NEXT_PUBLIC_PROTOCOL || "https"}://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 export const serverBaseUrl = `${process.env.PROTOCOL}://${process.env.VERCEL_URL}`;
