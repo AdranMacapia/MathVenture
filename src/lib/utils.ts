c92c5ff4725cd7eb56a909783e0b9fd687a9f843
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2024-04-19 04:13:43 +0100

    Account for prod

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,5 +4,11 @@ export const parseColor = (color?: Color["rgb"] | null) => {
   return `rgba(${color?.r}, ${color?.g}, ${color?.b}, ${color?.a})`;
 };
 
-export const baseUrl = `${process.env.NEXT_PUBLIC_PROTOCOL || "https"}://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-export const serverBaseUrl = `${process.env.PROTOCOL || "https"}://${process.env.VERCEL_URL}`;
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://www.math-venture.vercel.app"
+    : `${process.env.NEXT_PUBLIC_PROTOCOL || "https"}://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+export const serverBaseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://www.math-venture.vercel.app"
+    : `${process.env.PROTOCOL || "https"}://${process.env.VERCEL_URL}`;
